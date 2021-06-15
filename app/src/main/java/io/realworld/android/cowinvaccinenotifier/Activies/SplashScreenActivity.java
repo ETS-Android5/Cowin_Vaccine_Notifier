30cc17e75a1f95e89e03644240b2a6cf691d3c6c
Ayan <ayansarkar1810@gmail.com>
2021-06-16 01:33:15 +0530

    Splash Screen and notification icon done

---
+++ app/src/main/java/io/realworld/android/cowinvaccinenotifier/Activies/SplashScreenActivity.java
@@ -0,0 +1,50 @@
+package io.realworld.android.cowinvaccinenotifier.Activies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import io.realworld.android.cowinvaccinenotifier.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView imageDesign, imageLogo;
+    TextView logo, tag;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom);
+
+        imageDesign =findViewById(R.id.design);
+        imageLogo = findViewById(R.id.logo);
+        logo=findViewById(R.id.logo_name);
+        tag=findViewById(R.id.quote);
+
+        imageDesign.setAnimation(topAnim);
+        imageLogo.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        tag.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
